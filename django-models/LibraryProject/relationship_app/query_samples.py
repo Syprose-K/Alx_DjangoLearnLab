78386766863fd46faa3771c93db588327e382ab9
syprose-k <kichesyprose@gmail.com>
2025-11-16 14:10:50 +0300

    utilized books.all in query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,32 @@
 from .models import Author, Book, Library, Librarian
 
 
-#Query all books by a specific author
+#Query all books by a specific author.
 def get_books_by_author(author_name):
     """
-    Return a queryset of all books written by the given author name.
+    Return all books written by the given author name.
     """
-    return Book.objects.filter(author__name=author_name)
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.book_set.all()
+    except Author.DoesNotExist:
+        return Book.objects.none()
 
 
 #List all books in a library.
 def get_books_in_library(library_name):
     """
-    Return a queryset of all books in the given library name.
+    Return all books in the given library name.
     """
-    return Book.objects.filter(libraries__name=library_name)
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return Book.objects.none()
 
+
+#Retrieve the librarian for a library.
 def get_librarian_for_library(library_name):
     """
     Return the librarian for the given library name, or None if not found.
