59086ac7e95d80e56209304354ec0ec5d149ac24
syprose-k <kichesyprose@gmail.com>
2025-11-16 21:49:04 +0300

    checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
