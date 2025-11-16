c32f471b08101578b7c41170d6b08a1b6a1bb13b
syprose-k <kichesyprose@gmail.com>
2025-11-16 14:00:00 +0300

    updated query.samples

--- django-models/LibraryProject/query_samples.py
+++ django-models/LibraryProject/query_samples.py
@@ -1,20 +1,25 @@
 from .models import Author, Book, Library, Librarian
 
-def books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.books.all()
-    except Author.DoesNotExist:
-        return []
 
-def books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
+#Query all books by a specific author
+def get_books_by_author(author_name):
+    """
+    Return a queryset of all books written by the given author name.
+    """
+    return Book.objects.filter(author__name=author_name)
 
-def librarian_of_library(library_name):
+
+#List all books in a library.
+def get_books_in_library(library_name):
+    """
+    Return a queryset of all books in the given library name.
+    """
+    return Book.objects.filter(libraries__name=library_name)
+
+def get_librarian_for_library(library_name):
+    """
+    Return the librarian for the given library name, or None if not found.
+    """
     try:
         library = Library.objects.get(name=library_name)
         return library.librarian
