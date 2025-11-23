8a0e151032810f071128507f7fe8e594254b4216
syprose-k <kichesyprose@gmail.com>
2025-11-23 19:20:39 +0300

    removed @admin.register() decorator

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ("username", "email", "first_name", "last_name", "date_of_birth", "is_staff")
     search_fields = ("username", "email", "first_name", "last_name")
@@ -15,3 +13,5 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
     )
+
+admin.site.register(CustomUser, CustomUserAdmin)
