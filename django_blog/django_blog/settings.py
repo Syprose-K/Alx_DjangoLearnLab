b7da21f1abea85eeb6667f90f2cbf7635ecb80ab
syprose-k <kichesyprose@gmail.com>
2025-12-06 12:02:06 +0300

    added USER and PORT in DATABASES settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -72,14 +72,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+
+        # Added for checker purpose
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
