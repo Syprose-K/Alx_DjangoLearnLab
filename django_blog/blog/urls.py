cae7308c43fbe5b15d75f1d5502495883aa0e8b7
syprose-k <kichesyprose@gmail.com>
2025-12-07 22:15:31 +0300

    delete and update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
     PostListView,
     PostDetailView,
@@ -8,10 +9,12 @@ from .views import (
 )
 
 urlpatterns = [
+    path('', PostListView.as_view(), name='home'),
+    
+    # CRUD for posts
     path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
-
