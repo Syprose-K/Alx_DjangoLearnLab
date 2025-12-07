edb90229db32db64a17320aed2ce3aaae9047ec5
syprose-k <kichesyprose@gmail.com>
2025-12-07 21:58:58 +0300

    management features

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,3 +9,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 8}),
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,30 +1,50 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
-from .forms import RegisterForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
 
-class UserLoginView(LoginView):
-    template_name = 'blog/login.html'
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'   # blog/templates/blog/post_list.html
+    context_object_name = 'posts'
+    paginate_by = 10
 
-class UserLogoutView(LogoutView):
-    template_name = 'blog/logout.html'
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  # blog/templates/blog/post_detail.html
+    context_object_name = 'post'
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = RegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'   # blog/templates/blog/post_form.html
 
-@login_required
-def profile(request):
-    if request.method == "POST":
-        request.user.email = request.POST.get("email")
-        request.user.save()
-        return render(request, "blog/profile.html", {"success": True})
-    return render(request, "blog/profile.html")
+    def form_valid(self, form):
+        # set author to current user before saving
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # ensure the author remains unchanged
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'  # blog/templates/blog/post_confirm_delete.html
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
