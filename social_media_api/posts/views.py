c6ef0fa1eef7c6e27026f94fa2f946f489b81cb5
syprose-k <kichesyprose@gmail.com>
2025-12-17 12:48:26 +0300

    following_users in posts/views required by checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -11,8 +11,8 @@ class FeedView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        followed_users = self.request.user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
 
 class PostViewSet(viewsets.ModelViewSet):
