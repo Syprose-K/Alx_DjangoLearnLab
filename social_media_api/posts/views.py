d0d8af134ba12e9a488b606d5774a6d89b09059c
syprose-k <kichesyprose@gmail.com>
2025-12-18 12:30:50 +0300

    posts.views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -43,10 +43,7 @@ class LikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             return Response(
@@ -70,10 +67,7 @@ class UnlikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like = Like.objects.filter(
-            user=request.user,
-            post=post
-        ).first()
+        like = Like.objects.filter(user=request.user, post=post).first()
 
         if not like:
             return Response(
